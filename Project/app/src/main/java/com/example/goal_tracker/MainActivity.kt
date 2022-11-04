dd82b395893c0134558e97c0638faab9f8b555cc
Ian Kavanagh <iankavanagh53@gmail.com>
2022-11-04 23:10:19 +0000

    Temp Welcome UI
    * Set up initial Home page UI with temp buttons for Login and Register
    * Add logging to test that button clicks are working

--- Project/app/src/main/java/com/example/goal_tracker/MainActivity.kt
+++ Project/app/src/main/java/com/example/goal_tracker/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.goal_tracker
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity : AppCompatActivity()
+{
+    private var _loginButton: Button? = null
+    private var _registerButton: Button? = null
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        _loginButton = findViewById(R.id.login_button)
+        _registerButton = findViewById(R.id.register_button)
+
+        _loginButton?.setOnClickListener { Log.d("Logging App", "Login Button Clicked") }
+        _registerButton?.setOnClickListener { Log.d("Logging App", "Register Button Clicked") }
     }
 }
